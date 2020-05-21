59385713d1a02a19c5d2d64d33bd117bd26766c3
response4amit <65704872+response4amit@users.noreply.github.com>
2020-05-21 16:14:45 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -11,8 +11,14 @@ setup(
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['Anaplan', 'REST API'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'M2Crypto',
+          'base64',
+          'requests',
+          'json',
+          'os',
+          'jks',
+          'time',
+          'logging',
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
