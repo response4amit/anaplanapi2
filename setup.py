b5b7e516d218bb20551c04e56e15be13291a3347
response4amit <65704872+response4amit@users.noreply.github.com>
2020-05-21 19:49:40 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'anaplanapi2',         # How you named your package folder (MyLib)
   packages = ['anaplanapi2'],   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
+  version = '0.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Anaplan REST API v2 library for Python',   # Give a short description about your library
   author = 'Jesse Wilson',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/response4amit/anaplanapi2',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/response4amit/anaplanapi2/archive/v_05.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/response4amit/anaplanapi2/archive/v_06.tar.gz',    # I explain this later on
   keywords = ['Anaplan', 'REST API'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'cryptography',
